45743c74d9677aeb09571ef6ecad6bf74645758f
Mika Salmela <mtsalmel84@gmail.com>
2023-01-09 00:18:48 +0200

    Linear advance off

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2077,7 +2077,7 @@
  *
  * See https://marlinfw.org/docs/features/lin_advance.html for full instructions.
  */
-#define LIN_ADVANCE
+//#define LIN_ADVANCE
 #if ENABLED(LIN_ADVANCE)
   //#define EXTRA_LIN_ADVANCE_K // Add a second linear advance constant, configurable with M900.
   #define LIN_ADVANCE_K 0.7    // Unit: mm compression per 1mm/s extruder speed
